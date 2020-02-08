e66f0f246f784930d5dc94113dee65e6417c588c
Silas Stoffel <silasstofel@gmail.com>
2020-02-08 15:05:20 -0300

    documentando comando

--- node/comandos.sh
+++ node/comandos.sh
@@ -3,3 +3,6 @@ sudo docker build -t node/lab-node-alpine .
 
 # Levantando o Container
 sudo docker run -p 3000:3000 -d node/lab-node-alpine
+
+# Usando o docker composer
+sudo docker-compose up
--- node/index.js
+++ node/index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',  (req, res) => {
-    return res.send('Hello Word :D');
+    return res.send('Hello Word');
 });
 
 app.listen(PORT, HOST);
