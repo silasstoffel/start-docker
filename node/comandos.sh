72196c8ba8add584c5dd1e3197f404c3d6059eca
Silas Stoffel <silasstofel@gmail.com>
2020-02-08 14:53:51 -0300

    Criando container node

---
+++ node/comandos.sh
@@ -0,0 +1,5 @@
+# Rodando o docker file (o . é para representa onde está o Dockerfile)
+sudo docker build -t node/lab-node-alpine .
+
+# Levantando o Container
+sudo docker run -p 3000:3000 -d node/lab-node-alpine
